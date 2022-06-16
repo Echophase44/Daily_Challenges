dc7d55c138e9eb3a7ad96474cbc04c4466b98bc4
Nick <Echophase44@github.com>
2022-06-15 22:55:18 -0400

    Daily JS challenge

---
+++ Javascript/switch_itup.js
@@ -0,0 +1,37 @@
+function switchItUp(number) {
+  const expr = number.toString();
+  switch (expr) {
+    case "1":
+      return "One";
+      break;
+    case "2":
+      return "Two";
+      break;
+    case "3":
+      return "Three";
+      break;
+    case "4":
+      return "Four";
+      break;
+    case "5":
+      return "Five";
+      break;
+    case "6":
+      return "Six";
+      break;
+    case "7":
+      return "Seven";
+      break;
+    case "8":
+      return "Eight";
+      break;
+    case "9":
+      return "Nine";
+      break;
+    case "0":
+      return "Zero";
+      break;
+  }
+}
+
+switchItUp(1);
