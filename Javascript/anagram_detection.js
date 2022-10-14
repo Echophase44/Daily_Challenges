c3be4a05110870e2cbd2942c2e86f48c95f45fb3
Nick <Echophase44@github.com>
2022-10-14 14:44:30 -0400

    Added anagram_detection.js to challenges

---
+++ Javascript/anagram_detection.js
@@ -0,0 +1,10 @@
+var isAnagram = function (test, original) {
+  test = test.toLowerCase().split("").sort().join("");
+  original = original.toLowerCase().split("").sort().join("");
+
+  if (test === original) {
+    return true;
+  } else {
+    return false;
+  }
+};
